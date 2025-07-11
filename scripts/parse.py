ac3ee97a9a00412583edce9fc67140cedc3785ce
Akimio521 <akimio2333@gmail.com>
2025-07-11 10:04:02 +0800

    fix: 修复 jq-path 匹配问题

--- scripts/parse.py
+++ scripts/parse.py
@@ -28,17 +28,17 @@ def parse_jq_url(data: str) -> list[str]:
     :param data: loon插件脚本内容
     :return: 提取到的脚本url列表
     """
-    script_block_pattern = (
-        r"\[Script\](.*?)(?=\[|\Z)"  # 匹配[Script]到下一个[或文件结束
-    )
-    script_block = re.search(script_block_pattern, data, re.DOTALL)
+    # rewrite_block_pattern = (
+    #     r"\[Rewrite\](.*?)(?=\[|\Z)"  # 匹配[Rewrite]到下一个[或文件结束
+    # )
+    # rewrite_block = re.search(rewrite_block_pattern, data, re.DOTALL)
 
-    if not script_block:
-        return []
+    # if not rewrite_block:
+    #     return []
 
     # 从区块中提取所有jq-path
-    path_pattern = r"jq-path=(https?://[^\s,]+)"
-    paths = re.findall(path_pattern, script_block.group(1))
+    jq_path_pattern = r'jq-path="?(https?://[^\s,"]+)"?'
+    paths = re.findall(jq_path_pattern, data)
 
     return list(set(paths))  # 去重后返回
 
@@ -55,11 +55,13 @@ def parse_script_time(data: str) -> str:
 if __name__ == "__main__":
     from download import download_data
 
-    data = download_data(
-        "https://kelee.one/Tool/Loon/Lpx/NeteaseCloudMusic_remove_ads.lpx"
-    )
+    data = download_data("https://kelee.one/Tool/Loon/Lpx/Bilibili_remove_ads.lpx")
 
     script_paths = parse_script_url(data)
     print("提取到的脚本路径:")
     for path in script_paths:
         print(path)
+
+    print("\n提取到的JQ脚本路径:")
+    for jq_url in parse_jq_url(data):
+        print(jq_url)
